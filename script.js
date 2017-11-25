a85572f660bd25a60a586fe803405b84cccf417a
lukasz341 <lukasz341@tlen.pl>
2017-11-25 22:19:28 +0100

    inkrementacja i dekrementacja

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 //console.log(test);
-var app = React.createElement(App);
+var Counter = React.createClass ({
+    getInitialState: function (){
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function (){
+        this.setState ({
+            counter: this.state.counter + 1
+
+        });
+
+    },
+
+
+    decrement: function (){
+        this.setState ({
+            counter: this.state.counter - 1
+
+        });
+
+    },
+
+
+    render: function(){
+        return React.createElement ('div', {},
+            React.createElement('button', {onClick: this.increment}, 'Zwiększenie o 1'),
+            React.createElement('button', {onClick: this.decrement}, 'Zmniejszenie o 1'),
+            React.createElement('p', {}, 'Licznik ' +this.state.counter)
+    );
+    }
+
+});
+
+var app = React.createElement(Counter);
 ReactDOM.render(app, document.getElementById('app'));
