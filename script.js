bfb4356f29c12dce1a5b6b37f1d4b206e8c29d2c
lukasz341 <lukasz341@tlen.pl>
2017-11-28 19:58:51 +0100

    dodanie metod

--- script.js
+++ script.js
@@ -6,9 +6,33 @@ var Counter = React.createClass ({
         };
     },
     
-    componentWillMount: function (){
-        console.log(' tekst ')
-    },
+   
+        componentWillMount: function (){
+            console.log(' wywoływana w momencie inicjalizacji komponentu przed wykonaniem ')
+        },
+        
+        componentDidMount: function (){
+            console.log(' metoda wywoływana gdy nasz komponent widnieje już na stronie, możemy wczytać coś z serwera ')
+        },
+
+        componentWillReceiveProps: function (){
+            console.log(' aktualizowanie stanu na podstawie nadchodzących właściwości')
+        },
+        
+     
+        
+        componentWillUpdate: function (){
+            console.log(' wywoływana tylko do przygotowania na nadchodzące zmiany')
+        },
+        
+        componentDidUpdate : function (){
+            console.log(' możemy wykonać jakieś manipulacje na DOM bo komponent został już przerysowany')
+        },
+        
+        componentWillUnmount: function (){
+            console.log(' rzeczy związane z usuwaniem komponentu, odpinanie timerów czy nasłuchiwania zdarzeń na elementach dom')
+        },
+           
 
     increment: function (){
         this.setState ({
@@ -32,9 +56,9 @@ var Counter = React.createClass ({
         return React.createElement ('div', {},
             React.createElement('button', {onClick: this.increment}, 'Zwiększenie o 1'),
             React.createElement('button', {onClick: this.decrement}, 'Zmniejszenie o 1'),
-            React.createElement('p', {}, 'Licznik ' +this.state.counter)
+            React.createElement('p', {}, 'Licznik ' + this.state.counter)
     );
-    }
+    },
 
 });
 
